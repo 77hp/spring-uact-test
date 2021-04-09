768f5a5faacc18187b85c3e3ef02f31c98544ce8
77hp <ehdgus872@naver.com>
2021-04-09 16:43:50 +0900

    first commit

---
+++ src/main/java/com/uact/springuacttest/SpringUactTestApplication.java
@@ -0,0 +1,13 @@
+package com.uact.springuacttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUactTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUactTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uact/springuacttest/SpringUactTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uact.springuacttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUactTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
